3b806054a3b921c2b67e10ee6576a4f788d7057b
Octavio Delpupo <octavio.delpupo@gmail.com>
2026-05-06 12:23:04 -0300

    update sh.

--- install.sh
+++ install.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
-
+#
+# pos-os-postinstall.sh - Instalar e configura programas no Pop!_OS (20.04 LTS ou superior)
+#
+# Autor:         Octavio Delpupo
+#
+# ------------------------------------------------------------------------ #
+#
+# COMO USAR?
+#   $ ./install.sh
+#
+# ----------------------------- VARIÁVEIS ----------------------------- #
 set -e
 
-echo "🚀 Iniciando pós-instalação do Pop!_OS..."
+##URLS
+URL_VSCODE="https://code.visualstudio.com/thank-you?dv=linux64_deb"
 
-# Atualizar sistema
-sudo apt update && sudo apt upgrade -y
+##DIRETÓRIOS E ARQUIVOS
+DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
 
-echo "✅ Finalizado!"
+#CORES
+
+VERMELHO='\e[1;91m'
+VERDE='\e[1;92m'
+SEM_COR='\e[0m'
+
+#FUNÇÕES
+
+# Atualizando repositório e fazendo atualização do sistema
+
+apt_update(){
+  sudo apt update && sudo apt dist-upgrade -y
+}
+
+apt_update
+
+
+## finalização
+
+  echo -e "${VERDE}[INFO] - Script finalizado, instalação concluída! :)${SEM_COR}"
